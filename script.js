44d911d0404e0be6819d57c2cb5282647d70944e
Mauro.V <remanomauro@gmail.com>
2023-03-04 08:15:27 +0100

    creating vector flow field

--- script.js
+++ script.js
@@ -43,10 +43,17 @@ class Particles {
     context.stroke()
   }
   update(){
-    this.angle += 0.5
-    this.x += this.speedX + Math.sin(this.angle) * 10
-    this.y += this.speedY + Math.cos(this.angle) * 7
-    this.history.push({ x: this.x, y: this.y})
+    let x = Math.floor(this.x / this.effect.cellSize)
+    let y = Math.floor(this.y / this.effect.cellSize)
+    let index = y * this.effect.cols + x
+    this.angle = this.effect.flowField[index]
+
+    this.speedX = Math.cos(this.angle)
+    this.speedY = Math.sin(this.angle)
+    this.x += this.speedX
+    this.y += this.speedY
+    
+    this.history.push({x: this.x, y: this.y})
     if (this.history.length > this.maxLength){
       this.history.shift()
     }
@@ -59,9 +66,25 @@ class Effect {
     this.height = height
     this.particles = []
     this.numberOfParticles = 50
+    this.cellSize = 20
+    this.rows
+    this.cols
+    this.flowField = []
     this.init()
   }
   init(){
+    //create flow field effect
+    this.rows = Math.floor(this.height / this.cellSize)
+    this.cols = Math.floor(this.width / this.cellSize)
+    this.flowField = []
+    for (let y = 0; y < this.rows; y++){
+      for (let x = 0; x < this.cols; x++){
+        let angle = (Math.cos(x) + Math.sin(y)) * 2
+        this.flowField.push(angle)
+      }
+      console.log(this.flowField)
+    }
+
     //create particles
     for (let i = 0; i < this.numberOfParticles; i++){
       this.particles.push(new Particles(this))
